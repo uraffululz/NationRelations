8d29d45f32aef6c05cf11fa4a966cd6c3f8c9376
Unknown <randomsimpson@gmail.com>
2018-02-09 13:08:47 -0600

    Debris Destruction
    Set up script to destroy debris prefabs (OnCollisionEnter) and instantiate a "DebrisExplosion" prefab with a particle system.

---
+++ Assets/Scripts/Debris.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject.CompareTag("Nation")) {
+			
+		}
+
+		explosion = Instantiate (explosion, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
