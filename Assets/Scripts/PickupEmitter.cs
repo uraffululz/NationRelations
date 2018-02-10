b821ce799dd3435cb80d913d74d67659584837fb
Unknown <randomsimpson@gmail.com>
2018-02-10 14:29:09 -0600

    Funding
    Changed name of "DebrisEmission" script to "PickupEmitter".
    Implemented Funding prefabs (GameObject, added spawning function and variables to PickupEmitter script).
    Shield shrinks in scale over time, but can be increased by picking up Funding, which are launched from the "Nations" below. When it shrinks to "nothingness", the LevelManager loads the GameOver scene.
    Spent time tweaking rigidbodies and scripts to allow better feel of physics and timers.
    Basic scoring is implemented, but not incorporated much yet.

---
+++ Assets/Scripts/PickupEmitter.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupEmitter : MonoBehaviour {
+
+
+	public GameObject[] debrisFabs;
+	public GameObject debris;
+	private GameObject debrisClone;
+	public float debTimeStart;
+	public float debTimer;
+
+	public GameObject[] fundFabs;
+	public GameObject funding;
+	private GameObject fundClone;
+	public float fundTimeStart;
+	public float fundTimer;
+
+
+	void Start () {
+		debTimeStart = 1.0f;
+		debTimer = debTimeStart;
+
+		fundTimeStart = 3.0f;
+		fundTimer = fundTimeStart;
+	}
+	
+
+	void Update () {
+		if (debTimer > 0.0f) {
+			debTimer = debTimer - Time.deltaTime;
+		} else {
+			FireDebris ();
+			debTimer = debTimeStart;
+		}
+
+		if (fundTimer > 0.0f) {
+			fundTimer -= Time.deltaTime;
+		} else {
+			SendFunding ();
+			fundTimer = fundTimeStart;
+		}
+	}
+
+
+	void FireDebris () {
+		debris = debrisFabs [Random.Range (0, debrisFabs.Length)];
+		Vector3 debPos = new Vector3 (Random.Range (-6.0f, 6.0f), 11.0f, 0.0f);
+/*TODO MAYBE Change velocity to be relative to spawn position, adding positive x-plane velocity to those in the negative x-space,
+and negative velocity to those in positive x-space*/
+		//Vector3 debVel = new Vector3 (Random.Range (-13.0f * debPos.x, 5.0f / debPos.x), 0.0f, 0.0f);
+		Vector3 debRot = new Vector3 (Random.Range (0.0f, 10.0f), Random.Range (0.0f, 2.0f), Random.Range (0.0f, 10.0f));
+
+		//print ("Position: " + debPos);
+		//print ("Velocity: " + debVel);
+
+		debrisClone = Instantiate (debris, debPos, Quaternion.identity, gameObject.transform);
+		Rigidbody debRB = debrisClone.GetComponent<Rigidbody> ();
+		//debRB.AddForce (debVel);
+		debRB.AddTorque (debRot);
+	}
+
+
+	void SendFunding () {
+		funding = fundFabs [Random.Range (0, fundFabs.Length)];
+		Vector3 fundPos = new Vector3 (Random.Range (-6.0f, 6.0f), 1.0f, 0.0f);
+		//Vector3 fundRot = new Vector3 (Random.Range (0.0f, 10.0f), Random.Range (0.0f, 2.0f), Random.Range (0.0f, 10.0f));
+		Vector3 fundVel = new Vector3 (0.0f, 80.0f, 0.0f);
+/*TODO Manipulate values for fundVel^ and fundTimer^^, along with Funding prefab Rigidbody component (mass, drag, etc.) in the Editor
+ * Also, the Shrink rate on the Shield script is a factor, along with its moveSpeed.
+ * Repeat until it feels good (which I guess means until it feels intense, but generous, and fun)*/
+
+		fundClone = Instantiate (funding, fundPos, Quaternion.identity, gameObject.transform);
+		Rigidbody fundRB = fundClone.GetComponent<Rigidbody> ();
+		fundRB.AddForce (fundVel);
+		//fundRB.AddTorque (fundRot);
+	}
+}
--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour {
 
-	float moveSpeed = 5.0f;
+	public LevelManager lvlManager;
+
+	public int score = 0;
+
+	float moveSpeed = 7.5f;
 
 	void Start () {
 		
@@ -12,6 +16,27 @@ public class Shield : MonoBehaviour {
 	
 
 	void Update () {
+		Movement ();
+		Shrink ();
+	}
+
+
+	void OnCollisionEnter (Collision snatch) {
+		if (snatch.gameObject.tag == "Debris") {
+			CatchDebris ();
+		}
+	}
+
+
+	void OnTriggerEnter (Collider grab) {
+		if (grab.gameObject.tag == "Funding") {
+			Destroy (grab.gameObject);
+			CollectFunding ();
+		}
+	}
+
+
+	void Movement () {
 		Vector3 shieldPos = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
 
 		if (Input.GetKey(KeyCode.LeftArrow)) {
@@ -22,5 +47,30 @@ public class Shield : MonoBehaviour {
 
 		shieldPos.x = Mathf.Clamp (shieldPos.x, -6.0f, 6.0f);
 		this.transform.position = shieldPos;
+	}
+
+
+	void Shrink () {
+		transform.localScale -= new Vector3 (0.0f, 0.05f * Time.deltaTime, 0.0f);
+		if (transform.localScale.y <= 0.0f) {
+/*TODO
+ * For now, load "GameOver" scene when player Shield shrinks to nothing.
+ * Later on, maybe just load hidden canvas elements (Score text, along with score variables(Debris/WMDs caught, time lasted, etc.))
+ * all while disabling visible objects in the (still active) Game scene in order to see score clearly.
+ * Also include "Back to Start" button. */
+			lvlManager.LoadLevel ("GameOver");
+		}
+	}
+
+
+	void CatchDebris () {
+		score++;
+		print ("Score increased");
+	}
+
+
+	void CollectFunding () {
+		transform.localScale += new Vector3 (0.0f, 0.1f, 0.0f);
+		print ("Funding Acquired! Shield size increased!");
 	}
 }
